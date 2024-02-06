831adcd2b7455c548d149c54f28f96f4f6d5522c
CelesteMH <157657933+CelesteMH@users.noreply.github.com>
2024-02-06 00:13:17 +0000

    Updated

--- calc.py
+++ calc.py
@@ -1,1 +1,23 @@
+num1 = int(input("Enter your first number: "))
+num2 = int(input("Enter your second number: "))
+operator = input("Enter your operator: ")
 
+if operator == "+":
+  print(f"{num1} + {num2} = {num1+num2}")
+elif operator == "-":
+  print(f"{num1} - {num2} = {num1 - num2}")
+elif operator == "/":
+  if num2 != 0:
+    print(f"{num1} / {num2} = {num1 / num2}")
+  else:
+   print(f"{num1} / {num2} is undefined")
+elif operator == "*":
+  print(f"{num1} * {num2} = {num1 * num2}")
+elif operator == "**":
+  print(f"{num1} ** {num2} = {num1 ** num2}")
+elif operator == "//":
+  print(f"{num1} // {num2} = {num1 // num2}")
+elif operator == "%":
+  print(f"{num1} % {num2} = {num1 % num2}")
+else:
+  print("try another valid operator: +, -, *, **, /, //, %")
